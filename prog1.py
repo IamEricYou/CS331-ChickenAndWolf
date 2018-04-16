762bbb69b7b6872b495311dbc1869fc8f1b3d279
Eric You <bapsajura@naver.com>
2018-04-16 16:50:45 -0700

    reading files

--- prog1.py
+++ prog1.py
@@ -2,14 +2,24 @@
 import sys
 import numpy as np
 
-def main():
-    for args in sys.argv[1:]:
-        print args
+def read_file(initial):
+    with open(initial) as f:
+        line = f.read().splitlines()
 
-    initial_state = sys.argv[1]
-    goal_state = sys.argv[2]
+    #print(line)
+    return line
+
+def main():
+    if len(sys.argv) != 5:
+        print('Usage: < initial state file > < goal state file > < mode > < output file >')
+        return None
+
+    initial_state = read_file(sys.argv[1])
+    goal_state = read_file(sys.argv[2])
     mode = sys.argv[3]
     output = sys.argv[4]
+
+    
 
 if __name__ == "__main__":
     main()
